c7e2dc6456827fa14016e349dd29f1985b12fb22
Gabriel J Coronin <gabrielcoronin@gmail.com>
2021-01-20 17:14:41 -0300

    Add project files.

---
+++ src/FuncionalTest.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FuncionalTest.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/FuncionalTest.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FuncionalTest.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
