8b668494f1a74f85c55e26695c4d637e28a0e5f3
Gabriel J Coronin <gabrielcoronin@gmail.com>
2021-01-20 17:28:15 -0300

    feat: add notification

---
+++ src/FuncionalTest.Domain/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FuncionalTest.Domain.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public double Saldo { get; set; }
+    }
+}
---
+++ src/FuncionalTest.Domain/Notification/Message.cs
@@ -0,0 +1,20 @@
+﻿namespace FuncionalTest.Domain.Notification
+{
+    public class Message
+    {
+        public Message(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public Message(string value)
+        {
+            Key = "";
+            Value = value;
+        }
+
+        public string Key { get; private set; }
+        public string Value { get; private set; }
+    }
+}
